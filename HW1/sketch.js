2e92900cca7d0b0835d286c2b275f8039a7197bf
Eliascm17 <Elias.cmoreno17@gmail.com>
2019-09-11 22:12:31 -0500

    Trying to implement button push functionality.

--- HW1/sketch.js
+++ HW1/sketch.js
@@ -1,58 +1,72 @@
+let floor;
+let Buttons = [];
+//preloading function that load all files from assets folder
+
 //////////////////////////////////////////
 //Setup function that runs once
-function setup() {
+function setup(){
 	//canvas initialization
-	createCanvas(550, 700);
+	createCanvas(550, 800);
 
 	//reassigning variables to make writing code easier
+	floors = [];
+	let diam = 115;
 	let w = width;
 	let h = height;
 
 	//arrays for x & y values of the button numbers 1-6 on the interface
 	let xofNums = [w * .20, w * .50, w * .80, w * .20, w * .50, w * .80];
 	let yofNums = [h * .17, h * .17, h * .17, h * .37, h * .37, h * .37, h * .87, h * .87, h * .87];
+	let words = ['Open','Close','911'];
 
 	//Initializing the number button objects as an array
 	for(let i = 0; i < 6; i++){
-		numButtons[i] = new numButtons(xofNums[i], yofNums[i], 120, i+1);
+		Buttons[i] = new numButtons(xofNums[i], yofNums[i], i+1);
 	}
 
 	//Initializing the special button objects as an array
-	for(let i = 0; i<3; i++){
-		specButtons[i+6] = new specButtons(xofNums[i], yofNums[i], 120, null);
-	}
+	// for(let i = 6; i<9; i++){
+	// 	Buttons[i] = new numButtons(xofNums[i], yofNums[i], words[i]);
+	// }
 
 	//new instance for the Display
-	Disp = new Display(w*.1, h*.465, w*.8, h*.28);
-	
-}
+	// Disp = new Display(w*.1, h*.465, w*.8, h*.28);
+} 
+
 //////////////////////////////////////////
 //Class for the Buttons with numbers in the elevator
   class numButtons {
 	//basic class constructor for each button
-	constructor(x,y,d,label){
+	constructor(x,y,label){
 		this.x = x;
 		this.y = y;
-		this.d = d;
+		this.d = 115;
 		this.label = label;
 		this.alpha = 255;
 	}
 
 	//show method that allows the object to be seen
 	show(){
-		strokeWeight(3);
-		fill(250,this.alpha);//fill of the circle
-		circle(this.x, this.y, this.d);//circle
+		strokeWeight(4);
+		fill(255);//fill of the circle
+		circle(this.x, this.y, this.d);
 		fill(0);//font color
-		textSize(55);//text size
-		text(this.label, this.x-16, this.y+16); //location	
+		textSize(62);//text size
+		text(this.label, this.x-18, this.y+19); //location	
 	}
+
+	//For when the button gets pressed
 	press(){
-		fill(235, 213, 52);
+		strokeWeight(4);
+		fill(158, 21, 21);
+		circle(this.x, this.y, this.d);
+		fill(0);
+		textSize(62);
+		text(this.label, this.x-18, this.y+19);
 		//user feedback here
-		//sounds and other things HERE
 	}
   }
+
 //////////////////////////////////////////
 //Class for the buttons that aren't numbers i.e. open, close, emergency
 class specButtons {
@@ -76,7 +90,7 @@ class specButtons {
 }
 //////////////////////////////////////////
 //Class for the use of the display to give easy to understand information to the user
-	class Display {
+class Display {
 	  constructor(x1,x2,x3,x4){
 		  this.x1 = x1;
 		  this.x2 = x2;
@@ -97,33 +111,31 @@ class specButtons {
 function draw(){
 	background(130);
 	//displaying numbered buttons
-	for (let i = 0; i < numButtons.length+2; i++){
-		numButtons[i].show();
-	}
-	//displaying non-numbered buttons
-	for (let i = 0; i < specButtons.length + 2; i++) {
-		specButtons[i].show();
+	for (let i = 0; i < 6; i++){
+		Buttons[i].show();
 	}
 }
 
-
-//Implement the following two functions to create button effect / user feedback
+//Implement the following two functions to create button effect / userfeedback
 function mousePressed() {
-	for (let i = 0; i < numButtons.length; i++) {
-		d = dist(mouseX, mouseY, numButtons[i].x, numButtons[i].y);
-		if (d < 60) { 
-			numButtons[i].press();
+	text(Buttons[i].y, 100,100);
+	for (let i = 0; i < 6; i++) {
+		d = dist(mouseX, mouseY, Buttons[i].x, Buttons[i].y);
+		//if mouse click is within one of the buttons
+		if (d < 57.5) { 
+			text(d, 100,100);
+			Buttons[i].press();
+			// floors.push(i);
+			// ElevatorLogic();
+			//setTimeout(function(){ alert("Hello"); }, 3000); for sounds and other things 
 		}
 	}
 	//initiate user feedback here
   }
 //////////////////////////////////////////
-//second part in giving user feedback to the user
-function mouseReleased(){
-	// initiate sounds preceeding button press as well as the preceeding functions (logic) of the elevator
-}
-//////////////////////////////////////////
 
 function ElevatorLogic(){
-
+	 //go towards that floor and at some point let the button go back to white
 }
+
+
